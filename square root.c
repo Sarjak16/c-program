d910e5081edb61c840d2c71c01598d3860a14eb3
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-02-13 19:17:59 +0545

    Add files via upload

---
+++ square root.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	double a,b;
+	printf("write a number:\n");
+	scanf("%lf",&a);
+	b=sqrt(a);
+	printf("square root of given number is :%lf",b);
+	return 0;
+}
