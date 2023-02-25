a06b4a15b564b13067a4b97df29487f4208736f7
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-02-25 12:49:37 +0545

    Add files via upload

---
+++ Lab3/primeno.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	    int i,j,k;
+
+    for(i=100;i<=200;i++) {
+        k=0;
+        for(j=2;j<i/2;j++) {
+            if(i%j==0){
+                k=1;
+                break;
+            } 
+        }
+        if(k!=1) printf("%d ",i);
+    }
+    printf("\n");
+    return 0;
+}
