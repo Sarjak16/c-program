3ba7b36a872d42ea0f0fcb1e70717bdb006b0b05
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-03-08 10:12:24 +0545

    Add files via upload

---
+++ lab5/smallest_value_array.c
@@ -0,0 +1,31 @@
+//Create a function that takes an int array as argument and returns the smallest value in the array.
+#include <stdio.h>
+
+int small(int n[], int x);
+
+int main()
+{
+    int n, i;
+    printf("Enter how many numbers would you like to enter in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    for (i = 0; i < n; i++)
+    {
+        printf("Input in %d's place: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    printf("%d is the smallest in array.\n", small(arr, n));
+    return 0;
+}
+
+int small(int n[], int x)
+{
+    int check = n[0], i;
+    for (i = 1; i < x; i++)
+    {
+        if (n[i] < check)
+            check = n[i];
+    }
+    return check;
+}
