6bfa096b17fb7c11728e88b6c988f61bd0b49c69
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-03-15 20:45:21 +0545

    Add files via upload

---
+++ lab6/palindrome.c
@@ -0,0 +1,23 @@
+//WAP to input a word and check whether it is palindrome or not.
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char a[30], b[30];
+    printf("Enter a word: ");
+    scanf("%s", a);
+    strlwr(a);
+    int x = 0;
+    while (a[x] != '\0')
+    {
+        b[x] = a[x];
+        x++;
+    }
+    if (strcmp(a, b) == 0)
+        printf("The word is palindrome.\n");
+    else
+        printf("The word is not palindrome.\n");
+    return 0;
+}
