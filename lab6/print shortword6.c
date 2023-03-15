0a5910e2356dcd91ae9b8baa24691df84d56f094
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-03-15 20:45:55 +0545

    Add files via upload

---
+++ lab6/print shortword6.c
@@ -0,0 +1,21 @@
+//WAP to input 3 words and print the shortest word.
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char a[3][20];
+    printf("Enter three words: \n");
+    scanf("%s %s %s", a[0], a[1], a[2]);
+    int x, y, z;
+    x = strlen(a[0]);
+    y = strlen(a[1]);
+    z = strlen(a[2]);
+    if (x < y && x < z)
+        printf("%s is the shortest word.\n", a[0]);
+    else if (y < x && y < z)
+        printf("%s is the shortest word.\n", a[1]);
+    else
+        printf("%s is the shortest word.\n", a[2]);
+    return 0;
+}
