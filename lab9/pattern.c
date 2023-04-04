8003cbb5a8a96d6841f30d2fdc51144e3d95dcc3
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-04-04 21:51:02 +0545

    Add files via upload

---
+++ lab9/pattern.c
@@ -0,0 +1,18 @@
+//WAP to draw a square and a circle and fill a pattern in both of them.
+#include <graphics.h>
+#include <stdio.h>
+
+void main() {
+int gd = DETECT, gm;
+initgraph(&gd,&gm,"C:\\TURBOC3\\BGI");
+
+setfillstyle(7,3);
+rectangle(100,100,200,200);
+floodfill(150,150,15);
+
+setfillstyle(8,4);
+circle(400,150,50);
+floodfill(400,150,15);
+getch();
+closegraph();
+}
